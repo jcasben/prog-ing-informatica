489c254d112cf74ea136bc24e6a4eb7cb6f31257
jcasben <jcasben02@gmail.com>
2023-06-29 18:31:00 +0200

    [NEW] Added Exercise2.

---
+++ Exercise2/src/main/FilmGenre.java
@@ -0,0 +1,8 @@
+package main;
+
+/**
+ * Tipo enumerado que recoge los generos que puede tener un objeto de la clase Film
+ */
+public enum FilmGenre {
+    ACCION, AUTOR, CIFI, COMEDIA, DRAMA, POLICIACA, TERROR;
+}
