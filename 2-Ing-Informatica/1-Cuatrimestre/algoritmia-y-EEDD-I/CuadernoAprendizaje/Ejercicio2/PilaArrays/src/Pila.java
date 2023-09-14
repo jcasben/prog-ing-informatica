8054036f0e8739f243257662f9766288e431f647
jcasben <jcasben02@gmail.com>
2023-09-14 16:21:08 +0200

    [UPDATE: PilaArrays] Developed class Pila

--- 2-Ing-Informatica/1-Cuatrimestre/algoritmia-y-EEDD-I/CuadernoAprendizaje/Ejercicio2/PilaArrays/src/Pila.java
+++ 2-Ing-Informatica/1-Cuatrimestre/algoritmia-y-EEDD-I/CuadernoAprendizaje/Ejercicio2/PilaArrays/src/Pila.java
@@ -1,3 +1,43 @@
 public class Pila {
-    
+    private final int [] elements;
+    private int top;
+
+    public Pila(int length) {
+        this.elements = new int[length];
+        this.top = -1;
+    }
+
+    public void push(int element) throws FullStackException {
+        if (top == elements.length - 1) {
+            throw new FullStackException();
+        }
+        top++;
+        elements[top] = element;
+    }
+
+    public void pop() throws EmptyStackException {
+        if (top == -1) {
+            throw new EmptyStackException();
+        }
+        top--;
+    }
+
+    public int top() throws EmptyStackException {
+        if (top == -1) {
+            throw new EmptyStackException();
+        }
+        return elements[top];
+    }
+
+    private static class EmptyStackException extends Exception {
+        public EmptyStackException() {
+            super("La cola aún no contiene elementos.");
+        }
+    }
+
+    private static class FullStackException extends Exception {
+        public FullStackException() {
+            super("La cola ha llegado al máximo de su capacidad.");
+        }
+    }
 }
