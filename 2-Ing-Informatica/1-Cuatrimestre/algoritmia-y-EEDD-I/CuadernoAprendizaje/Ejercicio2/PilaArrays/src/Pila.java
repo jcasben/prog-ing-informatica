855318ab1b834ebc99f5151ec435ddf98f719a57
jcasben <jcasben02@gmail.com>
2023-09-14 16:56:14 +0200

    [UPDATE: PilaArrays] Commented class Pila.java

--- 2-Ing-Informatica/1-Cuatrimestre/algoritmia-y-EEDD-I/CuadernoAprendizaje/Ejercicio2/PilaArrays/src/Pila.java
+++ 2-Ing-Informatica/1-Cuatrimestre/algoritmia-y-EEDD-I/CuadernoAprendizaje/Ejercicio2/PilaArrays/src/Pila.java
@@ -4,10 +4,13 @@ public class Pila {
 
     public Pila(int length) {
         this.elements = new int[length];
+        //Como iniciamos la cola vacia, lo indicaremos con un -1 en la posicion del top de la pila
         this.top = -1;
     }
 
     public void push(int element) throws FullStackException {
+        //Si la posicion del top ya esta en el limite de elementos de la pila y realizamos una operacion de push,
+        // salta una excepcion.
         if (top == elements.length - 1) {
             throw new FullStackException();
         }
@@ -16,6 +19,7 @@ public class Pila {
     }
 
     public void pop() throws EmptyStackException {
+        //Si la pila esta vacia e intentamos realizar una operacion de pop, nos salta una excepcion.
         if (top == -1) {
             throw new EmptyStackException();
         }
@@ -23,6 +27,7 @@ public class Pila {
     }
 
     public int top() throws EmptyStackException {
+        //Si la cola esta vacia e intentamos consultar el elemento que esta en el top, salta una excepcion
         if (top == -1) {
             throw new EmptyStackException();
         }
