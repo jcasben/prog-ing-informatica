27dff370c055c12081dd0a7a6caed4f92e0abe7e
jcasben <jcasben02@gmail.com>
2023-09-20 19:51:19 +0200

    [NEW] Added main class to PilaReferencias

---
+++ 2-Ing-Informatica/1-Cuatrimestre/algoritmia-y-EEDD-I/CuadernoAprendizaje/Ejercicio2/PilaReferencias/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Pila<Integer> pila = new Pila<>();
+        pila.push(10);
+        pila.push(20);
+        try {
+            System.out.println(pila.top());
+            pila.pop();
+            System.out.println(pila.top());
+        } catch (Pila.EmptyStackException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
