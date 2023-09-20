dd27c696ecf50251534d2986b5984113cd8a10fc
jcasben <jcasben02@gmail.com>
2023-09-20 19:40:52 +0200

    [NEW] Added main class to PilaArrays

---
+++ 2-Ing-Informatica/1-Cuatrimestre/algoritmia-y-EEDD-I/CuadernoAprendizaje/Ejercicio2/PilaArrays/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Pila pila = new Pila(10);
+        try {
+            pila.push(10);
+            pila.push(24);
+            System.out.println(pila.top());
+            pila.pop();
+            System.out.println(pila.top());
+        } catch (Pila.FullStackException e) {
+            System.err.println(e.getMessage());
+        } catch (Pila.EmptyStackException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
