cd0352e8d5cca503915a44eb526eeeb6bb6a34c0
jcasben <jcasben02@gmail.com>
2023-09-23 14:08:51 +0200

    [NEW] Added area.c

---
+++ 2-Ing-Informatica/1-Cuatrimestre/sistemas-operativos-I/C-basico/area/area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    #define PI 3.141593 
+    float radius = 0;
+    printf("*** Cálculo del area de un círculo ***\nIntroduce el radio: ");
+    scanf("%f", &radius);
+    printf("El área del circulo de radio %.2f es: %.4f ", radius, radius * radius * PI);
+    
+    return 0;
+}
