c35e50508d03f8dab594c6012aa12d56475f7af3
jcasben <jcasben02@gmail.com>
2024-03-04 12:55:45 +0100

    [UPDATE] Updated program

--- 2-Ing-Informatica/2-Cuatrimestre/Algoritmia-y-EEDD-II/AlgoritmosAvanzados/EfficientSorting/src/Main.java
+++ 2-Ing-Informatica/2-Cuatrimestre/Algoritmia-y-EEDD-II/AlgoritmosAvanzados/EfficientSorting/src/Main.java
@@ -1,23 +1,24 @@
 public class Main {
-    public static void printArray(int [] array) {
-        for (int j : array) {
-            System.out.print(j + " ");
-        }
-        System.out.println();
+    public static void main(String[] args) {
+        int [] array = generateInvertedArray(600000);
+        long start = System.currentTimeMillis();
+        Sorting.quickSort(array, 0, array.length - 1);
+        System.out.println("Time: " + (System.currentTimeMillis() - start) + " ms");
     }
 
-    public static int[] generateRandomArray(int size) {
-        int [] array = new int[size];
-        for (int i = 0; i < size; i++) {
+    private static int[] generateRandomArray(int n) {
+        int [] array = new int[n];
+        for (int i = 0; i < n; i++) {
             array[i] = (int) (Math.random() * 100);
         }
         return array;
     }
 
-    public static void main(String[] args) {
-        int [] array = generateRandomArray(600000);
-        long start = System.currentTimeMillis();
-        Sorting.selectionSort(array);
-        System.out.println("Time: " + (System.currentTimeMillis() - start) + " ms");
+    private static int [] generateInvertedArray(int n) {
+        int [] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = n - i;
+        }
+        return array;
     }
 }
--- 2-Ing-Informatica/2-Cuatrimestre/Algoritmia-y-EEDD-II/AlgoritmosAvanzados/EfficientSorting/src/Sorting.java
+++ 2-Ing-Informatica/2-Cuatrimestre/Algoritmia-y-EEDD-II/AlgoritmosAvanzados/EfficientSorting/src/Sorting.java
@@ -1,15 +1,107 @@
 public class Sorting {
-    public static void selectionSort(int [] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[minIndex]) {
-                    minIndex = j;
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
                 }
             }
-            int temp = array[i];
-            array[i] = array[minIndex];
-            array[minIndex] = temp;
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    public static void quickSort(int [] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int [] arr, int low, int high) {
+        int pivot = getMedian(arr, low, high);
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    private static int getMedian(int [] arr, int l, int r) {
+        int m = (l + r) / 2;
+        if (arr[l] > arr[m]) {
+            int temp = arr[l];
+            arr[l] = arr[m];
+            arr[m] = temp;
+        }
+        if (arr[l] > arr[r]) {
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+        }
+        if (arr[m] > arr[r]) {
+            int temp = arr[m];
+            arr[m] = arr[r];
+            arr[r] = temp;
+        }
+        int temp = arr[m];
+        arr[m] = arr[r - 1];
+        arr[r - 1] = temp;
+
+        return arr[r - 1];
+    }
+
+    public static void mergeSort(int [] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    private static void merge(int [] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int [] L = new int[n1];
+        int [] R = new int[n2];
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[l + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            R[i] = arr[m + 1 + i];
+        }
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
         }
     }
 }
