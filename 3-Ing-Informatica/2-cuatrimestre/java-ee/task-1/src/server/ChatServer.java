1324798277a97890433f9d5dc66019a70ce3a7e5
jcasben <jcasben02@gmail.com>
2025-03-06 18:23:28 +0100

    [UPDATE]: added logging and comments

--- 3-Ing-Informatica/2-cuatrimestre/java-ee/task-1/src/server/ChatServer.java
+++ 3-Ing-Informatica/2-cuatrimestre/java-ee/task-1/src/server/ChatServer.java
@@ -1,5 +1,6 @@
 package src.server;
 
+import src.common.UserInfo;
 import src.common.packages.CustomPackage;
 
 import java.io.IOException;
@@ -7,18 +8,23 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * Server for a chat developed used Java.IO Sockets
+ * @author jcasben
+ */
 public class ChatServer {
     public static final int PORT = 9999;
     private static final List<ServerSideClientHandler> clientHandlers = new ArrayList<>();
 
-    public static void main(String[] args) {
-        new ChatServer().start();
-    }
+    private static final Logger LOGGER = Logger.getLogger(ChatServer.class.getName());
 
-    private void start() {
+    public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.println("Server running on port " + PORT + "...");
+            LOGGER.log(Level.INFO, "Server running on port " + PORT + "...");
+            // For each new connection, it creates a new handler for that connection in a new thread.
             while (true) {
                 Socket socket = serverSocket.accept();
                 ServerSideClientHandler handler = new ServerSideClientHandler(socket);
@@ -27,11 +33,16 @@ public class ChatServer {
                 }
                 new Thread(handler).start();
             }
-        } catch (IOException ioEx) {
-            ioEx.printStackTrace();
+        } catch (IOException e) {
+            LOGGER.log(Level.WARNING, "The following exception occurred (" + e.getMessage() + ").");
         }
     }
 
+    /**
+     * Broadcasts a {@link CustomPackage} to all the connected clients but the sender client.
+     * @param customPackage package to be sent.
+     * @param sender the client that wants to broadcast the message.
+     */
     public static void broadcast(CustomPackage customPackage, ServerSideClientHandler sender) {
         synchronized (clientHandlers) {
             for (ServerSideClientHandler clientHandler : clientHandlers) {
@@ -42,10 +53,23 @@ public class ChatServer {
         }
     }
 
+    /**
+     * Removes a handler from the server when a client disconnects.
+     * @param handler handler to be removed.
+     */
     public static void removeHandler(ServerSideClientHandler handler) {
         synchronized (clientHandlers) {
             clientHandlers.remove(handler);
         }
+    }
+
+    /**
+     * Maps the handlers to an array of {@link UserInfo}, that is the object that represents
+     * the main information about the user from that is connected with that handler.
+     * @return the array of {@link UserInfo}
+     */
+    public static UserInfo[] connectedUsers() {
+        return clientHandlers.stream().map(ServerSideClientHandler::getUser).toArray(UserInfo[]::new);
     }
 }
 
