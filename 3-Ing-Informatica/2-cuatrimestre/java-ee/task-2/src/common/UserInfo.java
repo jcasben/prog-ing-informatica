bbc6f0ff1b15673fa4a35f503e21132a00324997
jcasben <jcasben02@gmail.com>
2025-03-09 23:54:28 +0100

    [UPDATE: added parameters first message and user list

--- 3-Ing-Informatica/2-cuatrimestre/java-ee/task-2/src/common/UserInfo.java
+++ 3-Ing-Informatica/2-cuatrimestre/java-ee/task-2/src/common/UserInfo.java
@@ -2,12 +2,52 @@ package src.common;
 
 import java.io.Serializable;
 
-/**
- * Record for storing the basic information about a client user.
- *
- * @param id   user id generated at client start.
- * @param nick nickname selected by the user ("Anonymous" if no nickname selected).
- * @author jcasben
- */
-public record UserInfo(String id, String nick) implements Serializable {
+public class UserInfo implements Serializable {
+    private String id;
+    private String nick;
+    private boolean firstMessageReceived;
+    private boolean usersListSent;
+
+    public UserInfo() {
+        this.firstMessageReceived = false;
+    }
+
+    public UserInfo(String id, String nick) {
+        this.id = id;
+        this.nick = nick;
+        this.firstMessageReceived = false;
+        this.usersListSent = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public boolean isFirstMessageReceived() {
+        return firstMessageReceived;
+    }
+
+    public void setFirstMessageReceived(boolean firstMessageReceived) {
+        this.firstMessageReceived = firstMessageReceived;
+    }
+
+    public boolean isUsersListSent() {
+        return usersListSent;
+    }
+
+    public void setUsersListSent(boolean usersListSent) {
+        this.usersListSent = usersListSent;
+    }
 }
