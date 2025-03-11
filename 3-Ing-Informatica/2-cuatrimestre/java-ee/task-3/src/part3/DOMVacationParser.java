f555b64a8478f2c419c7a9343dd41783908f7e2a
jcasben <jcasben02@gmail.com>
2025-03-11 15:53:32 +0100

    [NEW]: added part 3 to task 3

---
+++ 3-Ing-Informatica/2-cuatrimestre/java-ee/task-3/src/part3/DOMVacationParser.java
@@ -0,0 +1,47 @@
+package src.part3;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class DOMVacationParser {
+    public static void main(String[] args) {
+        if (args.length < 1) System.out.println("Usage: DOMVacationParser holidays.xml");
+        else try {
+            int totalDays = 0;
+            File xmlFile = new File(args[0]);
+
+            DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+            Document xmlDocument = documentBuilder.parse(xmlFile);
+
+            NodeList nodeList = xmlDocument.getElementsByTagName("vacation");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    String year = element.getElementsByTagName("year").item(0).getTextContent();
+                    String place = element.getElementsByTagName("place").item(0).getTextContent();
+                    Element placeElement = (Element) element.getElementsByTagName("place").item(0);
+                    String country = placeElement.getAttribute("country");
+                    String duration = element.getElementsByTagName("duration").item(0).getTextContent();
+                    totalDays += Integer.parseInt(duration);
+
+                    System.out.println(year + ": " + place + " (" + country + ") - Duration: " + duration);
+                }
+            }
+            System.out.println("\nTotal vacation days: " + totalDays);
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
